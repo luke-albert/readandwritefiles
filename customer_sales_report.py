bf3bbd58a937ab3d46669eced0a57152f5d33607
Luke <luke_albert1@baylor.edu>
2023-01-30 21:57:59 -0600

    sales csv update

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -1,38 +1,27 @@
 import csv
 
 
-salesfile = "sales.csv"
-
-infile = open(salesfile, "r", newline="")
-reader = csv.reader(infile, delimiter=",")
-next(reader)
-
-outfile = open("salesreport.csv", "w", newline="")
-writer = csv.writer(outfile, delimiter=" ")
-header = ["Customer", "|" "Total"]
-writer.writerow(header)
+infile = csv.reader(open("sales.csv", "r", newline=""))
+next(infile)
+outfile = open("salesreport.csv", "w")
 
 
-cust_id = 250
-subtotal = 0
-tax = 0
-freight = 0
+cust_id = "250"
+customer_total = 0
 
+outfile.write("Customer  |  Total\n")
 
-for row in reader:
-    if float(row[0]) == cust_id:
-        subtotal += float(row[3])
-        tax += float(row[4])
-        freight += float(row[5])
-    else:
-        total = subtotal + tax + freight
-        custnum = int(row[0])
-        newinfo = [[custnum, total]]
-        writer.writerows(newinfo)
-        cust_id = int(row[0])
-        subtotal = float(row[3])
-        tax = float(row[4])
-        freight = float(row[5])
+for row in infile:
+    if cust_id != row[0]:
+        outfile.write("\t" + cust_id + "\t\t" + str(customer_total) + "\n")
+
+        cust_id = row[0]
+        customer_total = 0
+
+    total = float(row[3]) + float(row[4]) + float(row[5])
+    customer_total += total
+
+outfile.write("\t" + cust_id + "\t\t" + str(customer_total + "\n"))
 
 
 outfile.close()
